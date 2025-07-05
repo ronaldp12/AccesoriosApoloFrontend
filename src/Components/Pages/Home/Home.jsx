d8f4bef619a43999451ab7cb1e6bae786c19bfd0
Ronald patiño <patinoguevararonald@gmail.com>
2025-07-05 10:24:19 -0500

    links redes sociales agregado

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Slider } from '../../Layouts/Slider/Slider.jsx';
 import { DiscountSection } from '../../Layouts/DiscountSection/DiscountSection.jsx';
-import { AccessoriesSelector } from '../../Layouts/AccesoriesSelector/AccesoriesSelector.jsx';
 import { BestSellersSection } from '../../Layouts/BestSellerSection/BestSellerSection.jsx';
 import { JoinBanner } from '../../Layouts/JoinBanner/JoinBanner.jsx';
 import { FeaturedBrands } from '../../Layouts/FeaturedBrands/FeauturedBrands.jsx';
@@ -34,7 +33,6 @@ export const Home = () => {
 
       <Slider />
       <DiscountSection />
-      <AccessoriesSelector />
       <BestSellersSection onAddToCart={handleAddToCart} />
       <JoinBanner
         onOpenRegister={openRegister}
--- src/Components/Ui/FooterBottom/FooterBottom.jsx
+++ src/Components/Ui/FooterBottom/FooterBottom.jsx
@@ -4,13 +4,16 @@ export const FooterBottom = () => (
     <div className="footer-bottom">
         <div className="footer-bottom-content">
             <Logo />
-            <span>ACCESORIOS APOLO - 2024</span>
+            <span>ACCESORIOS APOLO - 2025</span>
         </div>
 
         <div className="social-icons-footer-bottom">
-            <i className="hgi hgi-stroke hgi-facebook-02"></i>
-            <i className="hgi hgi-stroke hgi-instagram"></i>
-            <i className="hgi hgi-stroke hgi-new-twitter"></i>
+            <a href="https://www.facebook.com/profile.php?id=61574186018322&locale=es_LA" target="_blank" rel="noopener noreferrer">
+                <i className="hgi hgi-stroke hgi-facebook-02"></i>
+            </a>
+            <a href="https://www.instagram.com/accesoriosapolom/" target="_blank" rel="noopener noreferrer">
+                <i className="hgi hgi-stroke hgi-instagram"></i>
+            </a>
         </div>
     </div>
 );
